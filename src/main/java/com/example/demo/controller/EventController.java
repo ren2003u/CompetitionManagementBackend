102b9b170365aa917084d6e08013e40e68460da2
jiahao ren <542251108@qq.com>
2023-05-29 19:19:16 +0800

    enhance the security validation in EventController

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
-
+import java.util.Objects;
 
 
 @RestController
@@ -39,10 +39,9 @@ public class EventController {
     }
 
     // 2. Look up event information for a specific event name
-    @RequestMapping("/byname/{event_name}")
+    @RequestMapping("/byName/{event_name}")
     @ApiOperation(value = "根据活动名称查询活动信息", notes = "根据给定的活动名称查找活动信息")
     public HashMap<String, Object> getEventByName(@PathVariable("event_name") String event_name) {
-        //todo: The return value is incomplete
         if(StringUtils.isBlank(event_name)){
             return AjaxResult.fail(-1,"传输的活动名称非法");
         }
@@ -54,27 +53,60 @@ public class EventController {
     }
 
     // 3. Modify or add event information
-    @RequestMapping("/addOrUpdate")
-    @ApiOperation(value = "添加或更新活动信息", notes = "根据给定的活动信息添加新活动或更新已有活动信息")
-    public HashMap<String, Object> addOrUpdateEvent(@RequestBody EventInformation eventInformation) {
+//    @RequestMapping("/addOrUpdate")
+//    @ApiOperation(value = "添加或更新活动信息", notes = "根据给定的活动信息添加新活动或更新已有活动信息")
+//    public HashMap<String, Object> addOrUpdateEvent(@RequestBody EventInformation eventInformation) {
+//        if(StringUtils.isBlank(eventInformation.getEvent_location()) || StringUtils.isBlank(eventInformation.getEvent_name()) || StringUtils.isBlank(eventInformation.getEvent_time())){
+//            return AjaxResult.fail(-1,"传输的活动信息不完整或含有非法信息");
+//        }
+//        EventInformation existingEvent = eventInformationService.findEventByName(eventInformation.getEvent_name());
+//        if (existingEvent != null) {
+//            // Update event information
+//            existingEvent.setEvent_name(existingEvent.getEvent_name());
+//            existingEvent.setEvent_location(eventInformation.getEvent_location());
+//            existingEvent.setEvent_time(eventInformation.getEvent_time());
+//            eventInformationService.updateEvent(existingEvent);
+//        } else {
+//            // Add new event
+//            EventInformation newEvent = new EventInformation(0, eventInformation.getEvent_name(), eventInformation.getEvent_time(), eventInformation.getEvent_location());
+//            eventInformationService.addEvent(newEvent);
+//        }
+//        return AjaxResult.success(200, "赛事信息操作成功");
+//    }
+
+    @RequestMapping("/addEvent")
+    @ApiOperation(value = "添加活动信息", notes = "根据给定的活动信息添加新活动")
+    public HashMap<String, Object> addEvent(@RequestBody EventInformation eventInformation) {
         if(StringUtils.isBlank(eventInformation.getEvent_location()) || StringUtils.isBlank(eventInformation.getEvent_name()) || StringUtils.isBlank(eventInformation.getEvent_time())){
             return AjaxResult.fail(-1,"传输的活动信息不完整或含有非法信息");
         }
-        EventInformation existingEvent = eventInformationService.findEventByName(eventInformation.getEvent_name());
-        if (existingEvent != null) {
-            // Update event information
-            existingEvent.setEvent_name(existingEvent.getEvent_name());
-            existingEvent.setEvent_location(eventInformation.getEvent_location());
-            existingEvent.setEvent_time(eventInformation.getEvent_time());
-            eventInformationService.updateEvent(existingEvent);
-        } else {
-            // Add new event
-            EventInformation newEvent = new EventInformation(0, eventInformation.getEvent_name(), eventInformation.getEvent_time(), eventInformation.getEvent_location());
-            eventInformationService.addEvent(newEvent);
+        EventInformation event = eventInformationService.findEventByName(eventInformation.getEvent_name());
+        if (event != null) {
+            return AjaxResult.fail(-1, "活动名称已存在");
         }
+            // Add new event
+            eventInformationService.addEvent(eventInformation);
         return AjaxResult.success(200, "赛事信息操作成功");
     }
 
+    @RequestMapping("/updateEvent")
+    @ApiOperation(value = "添加活动信息", notes = "根据给定的活动信息添加新活动")
+    public HashMap<String, Object> updateEvent(@RequestBody EventInformation eventInformation) {
+        if(StringUtils.isBlank(eventInformation.getEvent_location()) || StringUtils.isBlank(eventInformation.getEvent_name()) || StringUtils.isBlank(eventInformation.getEvent_time())){
+            return AjaxResult.fail(-1,"传输的活动信息不完整或含有非法信息");
+        }
+        EventInformation event = eventInformationService.findEventByName(eventInformation.getEvent_name());
+        if (event != null) {
+            return AjaxResult.fail(-1, "活动名称已存在");
+        }
+        if(!Objects.equals(eventInformationService.findEventByNumber(eventInformation.getEvent_number()).getEvent_name(), eventInformation.getEvent_name())){
+            if(eventInformationService.findEventByName(eventInformation.getEvent_name())!= null){
+                return AjaxResult.fail(-1,"要更新的活动名称已经被使用");
+            }
+        }
+        eventInformationService.addEvent(eventInformation);
+        return AjaxResult.success(200, "赛事信息操作成功");
+    }
     // 4. Delete the event information according to the specified event number
     @RequestMapping("/delete/{event_number}")
     @ApiOperation(value = "根据活动编号删除活动信息", notes = "根据指定的活动编号删除活动信息")
