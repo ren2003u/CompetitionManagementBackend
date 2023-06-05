d369a0eccbb7ca3e1c8d6392b11368af7df5d2fa
jiahao ren <542251108@qq.com>
2023-06-05 23:31:37 +0800

    add essential comment into Jwt related classes

--- src/main/java/com/example/demo/auth/jwt/JwtRequestFilter.java
+++ src/main/java/com/example/demo/auth/jwt/JwtRequestFilter.java
@@ -58,8 +58,29 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         // Once we get the token validate it.
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Question 1: What is SecurityContextHolder.getContext().getAuthentication() == null used for?
+            // Answer: SecurityContextHolder is a component of Spring Security that stores details about the current security context, including details about the current user.
+            // SecurityContextHolder.getContext().getAuthentication() returns the Authentication object stored in the security context.
+            // This Authentication object represents the currently authenticated user, if any.
+            // If getAuthentication() returns null, it means that no authentication information is stored in the security context, or in other words, the user is not authenticated yet.
+            // This check is done to ensure that Spring Security does not perform unnecessary authentication operations if the user is already authenticated.
+
+
+            // Question 2: What does this line of code do?
+            // Answer: The UserDetailsService is another component of Spring Security.
+            // It is used to retrieve the user's details from the database.
+            // The loadUserByUsername method loads a user record based on the username and returns a fully populated UserDetails object, which includes the username, password, and a set of authorities (roles).
+            // The UserDetails object and the JWT token are used together to validate the token because the token was originally created for a specific user (the username is often included as a subject in the token).
+            // The validation checks that the token was issued for the user that's currently trying to access the API, and that the token is not expired.
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
             if (jwtUtil.validateToken(jwtToken, userDetails)) {
+                // Question 3: Why do we create UsernamePasswordAuthenticationToken?
+                // Answer: UsernamePasswordAuthenticationToken is a specific implementation of the Authentication interface, which Spring Security uses to represent the token for a user that's authenticated with a username and password.
+                // When you create a new instance of UsernamePasswordAuthenticationToken and put it in the SecurityContext, you're telling Spring Security that the current user is authenticated.
+                // This object not only includes the user's username and password but also includes the user's roles (authorities), which Spring Security uses for access control checks.
+                // In your code, you're passing null as the credentials because the password is not needed after the user is authenticated, and the user's roles are loaded from the UserDetails object.
+                // After setting this token in the SecurityContext, subsequent code (including your API handlers) can trust that the user is authenticated.
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
--- src/main/java/com/example/demo/auth/jwt/JwtUtil.java
+++ src/main/java/com/example/demo/auth/jwt/JwtUtil.java
@@ -57,6 +57,17 @@ public class JwtUtil {
     }
 
     // To retrieve any information from token we will need the secret key
+    // Question:I am curious if we skip the login part and fabricate a similar format token.
+    // Then we fill this fake token into our request header and try to access those protected APIs.
+    // how do the SpringSecurity knows the token is invalid?
+    // Answer:Spring Security uses the secret key to validate the JWT token.
+    // As part of the validation process, Spring Security checks that the token was signed using the same secret key that's stored in the server.
+    // If the token was not signed with the correct secret key, it will be rejected.
+    // Let's take a look at the method where this happens in your JwtUtil class:
+    // Here, Jwts.parser().setSigningKey(secret) creates a JWT parser that will use the secret key to validate the token.
+    // parseClaimsJws(token) parses the JWT token, validates its signature, and returns the claims inside the token.
+    // If the signature is not valid (i.e., the token was not signed with the correct secret key), parseClaimsJws will throw an exception, and the token will be rejected.
+    // This is why it's very important to keep your secret key private. Anyone who knows the secret key can generate valid tokens, which would be a serious security risk.
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
